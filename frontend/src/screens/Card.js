553776ef0cb8673711a606f32efe7d9ac040e30a
Cay <cay.henning@proton.me>
2023-07-08 12:52:37 -0400

    Fixes to responsiveness

--- frontend/src/screens/Card.js
+++ frontend/src/screens/Card.js
@@ -7,7 +7,7 @@ const Card = ({...props}) => {
 
     useEffect(() => {
         // Set width of card depending on # of cartoons in scroller
-        setClassName("w-[" + 100*(1/props.cartoonsPerPage) + "%]");
+        setClassName(Math.floor(100*(1/props.cartoonsPerPage)));
     }, [props.cartoonsPerPage])
 
     useEffect(() => {
@@ -15,28 +15,28 @@ const Card = ({...props}) => {
     }, [props.cartoon])
 
     return (
-        <div className={"flex flex-col " + className}>
+        <div style={{width: className + '%'}}>
             <Link 
                 id={cartoon._id} 
                 title={cartoon.title}
                 to={"/cartoons/" + cartoon.id}
-                className={"flex-grow"}>
-                <img src={cartoon.picture} className={'w-full h-full rounded-md hover:shadow-lg hover:shadow-rose-200 object-cover'}/>
+                className={"h-[90%] block"}>
+                <img src={cartoon.picture} className={'h-full rounded-md hover:shadow-lg hover:slay-sm object-cover object-top m-auto'}/>
             </Link>
-            <p className="flex flex-row p-1 gap-1" >
-                <div className="flex flex-row p-1 gap-1" title={"Favorited " + cartoon.nfavorites + " times"}>
-                    <svg className="w-4 h-4 text-red-600 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+            <div className="flex flex-row p-1 gap-1 shrink-0 justify-center" >
+                <div className="flex flex-row gap-1 justify-center items-center" title={"Favorited " + cartoon.nfavorites + " times"}>
+                    <svg className="w-3 h-3 text-red-600 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                         <path d="M17.947 2.053a5.209 5.209 0 0 0-3.793-1.53A6.414 6.414 0 0 0 10 2.311 6.482 6.482 0 0 0 5.824.5a5.2 5.2 0 0 0-3.8 1.521c-1.915 1.916-2.315 5.392.625 8.333l7 7a.5.5 0 0 0 .708 0l7-7a6.6 6.6 0 0 0 2.123-4.508 5.179 5.179 0 0 0-1.533-3.793Z"/>
                     </svg>
-                    <small>{cartoon.nfavorites}</small>
+                    <small className="text-xs">{cartoon.nfavorites}</small>
                 </div>
-                <div className="flex flex-row p-1 gap-1" title={"Average Rating: " + cartoon.avgrating + "%"}>
-                    <svg className="w-4 h-4 text-orange-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <div className="flex flex-row gap-1 justify-center items-center" title={"Average Rating: " + cartoon.avgrating + "%"}>
+                    <svg className="w-3 h-3 text-orange-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                         <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
                     </svg>
                     <small>{cartoon.avgrating}%</small>
                 </div>
-            </p>
+            </div>
         </div>
     )
 }
--- frontend/src/screens/Scroller.js
+++ frontend/src/screens/Scroller.js
@@ -21,7 +21,19 @@ const Scroller = ({...props}) => {
     // Change number of cartoons in scroller depending on window width
     const handleResize = () => {
         let innerWidth = window.innerWidth;
-        if (innerWidth > 1280)
+        if (innerWidth > 1920)
+        {
+            setCartoonsPerPage(15);
+        }
+        else if (innerWidth > 1480)
+        {
+            setCartoonsPerPage(9);
+        }
+        else if (innerWidth > 1280)
+        {
+            setCartoonsPerPage(7);
+        }
+        else if (innerWidth > 1080)
         {
             setCartoonsPerPage(6);
         }
@@ -29,19 +41,41 @@ const Scroller = ({...props}) => {
         {
             setCartoonsPerPage(5);
         }
-        else
+        else if (innerWidth > 400)
         {
             setCartoonsPerPage(3);
+        }
+        else
+        {
+            setCartoonsPerPage(1);
         }
     }
 
     useEffect(() => {
-        // Update current cartoons when page or number of cartoons per page changes
+        // Update current cartoons when number of cartoons per page changes
+        let page = currentPage;
+        let max = Math.ceil((cartoons.length / cartoonsPerPage));
+        // If page expanding causes current page to be greater than the max
+        if (currentPage > max)
+        {
+            page = max;
+            setCurrentPage(page);
+        }
+        setMaxPages(max);
+
+        updateCartoons()
+    }, [cartoonsPerPage])
+
+    useEffect(() => {
+        updateCartoons()
+    }, [currentPage])
+
+    const updateCartoons = () => {
+        // Update current cartoons when page number changes
         let lastIndex = currentPage * cartoonsPerPage;
         let firstIndex = lastIndex - cartoonsPerPage;
-        setMaxPages(Math.ceil((cartoons.length / cartoonsPerPage)));
         setCurrentCartoons(cartoons.slice(firstIndex, lastIndex));
-    }, [currentPage, cartoonsPerPage])
+    }
 
     const paginateLeft = () => {
         setAnimationClass("invisible translate-x-[120%]")
@@ -54,8 +88,8 @@ const Scroller = ({...props}) => {
 
             setTimeout(() => {
                 setAnimationClass("translate-x-[0]")
-            }, 200)
-        }, 300)
+            }, 100)
+        }, 100)
     }
 
     const paginateRight = () => {
@@ -65,8 +99,8 @@ const Scroller = ({...props}) => {
             setCurrentPage((currentPage % maxPages) + 1);
             setTimeout(() => {
                 setAnimationClass("translate-x-[0]")
-            }, 200)
-        }, 300)
+            }, 100)
+        }, 100)
     }
 
     let cartoonRender = currentCartoons.map((cartoon, index) => {
@@ -77,7 +111,7 @@ const Scroller = ({...props}) => {
     })
 
     return (
-        <div className="flex flex-row gap-5 h-72 place-space-between overflow-x-hidden">
+        <div className="flex flex-row gap-5 h-72 overflow-x-hidden p-4">
             <div className="flex flex-column justify-center">
                 <button onClick={paginateLeft} className='h-12 w-12 m-auto hover:bg-gray-100 rounded-full'>
                 <svg className="w-6 h-6 text-gray-800 dark:text-white m-auto" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
@@ -85,7 +119,7 @@ const Scroller = ({...props}) => {
                 </svg>
                 </button>
             </div>
-            <div className={"w-full flex flex-row gap-4 h-72 transition-{translate} duration-300 " + animationClass}>
+            <div className={"w-full flex flex-row justify-start gap-4 h-full transition-{translate} duration-100 " + animationClass}>
                 { cartoonRender }
             </div>
             <div className="flex flex-column justify-center">
