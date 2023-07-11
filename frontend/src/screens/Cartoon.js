d3d7021d0493c5e69206b484278dbe3a577c30f4
Nicole Zolnier <nzolnier@gmail.com>
2023-07-11 00:30:55 -0400

    error page

--- frontend/src/screens/Cartoon.js
+++ frontend/src/screens/Cartoon.js
@@ -1,5 +1,14 @@
 const Cartoon = () => {
-    return <div>Cartoon component</div>
+    return <div>
+        <div>bg</div>
+        <div>img rating stuff</div>
+        <div>
+            cartoon info
+        </div>
+        <div>
+            most recent
+        </div>
+    </div>
 }
 
 export default Cartoon
