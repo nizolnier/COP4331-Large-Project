7d5245e23980a518526c34879fb7be0316f22bca
Mike Burke <michaelburke.eh@gmail.com>
2023-07-12 21:15:13 -0400

    removal of unecessary divs

--- frontend/src/screens/Profile.js
+++ frontend/src/screens/Profile.js
@@ -7,6 +7,9 @@ import { baseUrl } from '../constants/url.js'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { useMediaQuery } from 'react-responsive';
+import Scroller from '../components/Scroller'
+import sampleCartoons from '../tests/sample_shows.json'
+import Header from '../components/Header'
 
 const Profile = () => {
     const user = {
@@ -24,20 +27,23 @@ const Profile = () => {
 
     return (
 
-        <div className="flex flex-col w-screen bg-[#1F1D36]">
-            <div className="w-screen h-[250px] bg-cover z-0"
+        <div className="flex w-screen h-auto bg-[#1F1D36]">
+            <div className="flex flex-col">
+                {toggle || !isMobile ? <Sidebar username={user.username} name={user.name} userid={user.userid} /> : <></>}
+            </div>
+            
+            <div className="w-1/6"></div>
+            <div className={`justify-center ${!isMobile ? 'w-5/6 ' : 'w-6/6'}`}>
+                
+                <div className="w-[100] h-[300px] bg-cover z-0"
                     style={{ backgroundImage: `linear-gradient(180deg, transparent 0%, #1F1D36 97%), url(${isMobile ? pm : pw})` }}>
                 </div>
-            <div>
-                <div>
-                    {toggle || !isMobile ? <Sidebar username={user.username} name={user.name} userid={user.userid} /> : <></>}
-                </div>
-                <div className="flex w-screen items-center justify-center">
+                <div className="flex w-[100] items-center justify-center">
                     <div class="w-20 h-20 rounded-full flex justify-center items-center bg-stone-300">
                         404
                     </div>
                 </div>
-                <div className="flex flex-col justify-center items-center w-screen h-[15%] bg-[#1F1D36]">
+                <div className="flex flex-col justify-end items-center w-[100] h-[15%] bg-[#1F1D36]">
                     <h1 className="w-screen pb-8 text-center justify-center text-white text-lg lg:text-3xl font-bold tracking-wide">Hi, {user.name}!</h1>
                     <div className="flex flex-row  w-screen h-[15%] items-center justify-center">
                         <div className="TotalFilms flex flex-col basis-28 items-center justify-center">
@@ -50,20 +56,11 @@ const Profile = () => {
                         </div>
                     </div>
                 </div>
-                <h1 className="w-screen my-8 pb-8 text-center justify-center text-white text-lg lg:text-3xl font-bold tracking-wide">Your Favorites</h1>
-                <div className="flex flex-row justify-center items-center w-screen">
-
-
-                </div>
-                <div className="flex basis-36 flex-col justify-center items-center w-screen">
-                    <h1 className="w-screen my-8 pb-8 text-center justify-center text-white text-lg lg:text-3xl font-bold tracking-wide">Your Watchlist</h1>
-                </div>
-                <div className="flex flex-row justify-center items-center w-screen">
-
-                </div>
-                <div className="w-screen h=10 flex my-8">
-
-                </div>
+                <h1 className="w-[100] mt-4 -mb-12 pb-8 text-center justify-center text-white text-lg lg:text-3xl font-bold tracking-wide">Your Favorites</h1>
+                <Scroller className="" cartoons={sampleCartoons} heading="Your Favorites"></Scroller>
+                <h1 className="w-[100] mt-4 -mb-12 pb-8 text-center justify-center text-white text-lg lg:text-3xl font-bold tracking-wide">Your Watchlist</h1>
+                <Scroller className="" cartoons={sampleCartoons} heading="Your Watchlist"></Scroller>
+                <div className="w-[100] h=400 flex my-8 "></div>
             </div>
         </div>
 
