a29c7fab50206444d9ac97006fa550f22d372572
Cay <cay.henning@proton.me>
2023-07-18 12:37:33 -0400

    add pagination

--- backend/controllers/searchShows.js
+++ backend/controllers/searchShows.js
@@ -6,24 +6,15 @@ const SearchShows = asyncHandler(async (req, res) => {
   const { page } = req.query.page;
   const { limit } = req.query.limit;
 
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
   const results = await Show.find({ title: query })
+  let pageResults = null;
 
   if (results) {
-    res.status(200).json(results.map(showCur => {
-        return {
-            showid: showCur.showid,
-            picture: showCur.picture,
-            title: showCur.title,
-            genre: showCur.genre,
-            description: showCur.description,
-            year: showCur.year,
-            director: showCur.director,
-            num_of_ratings: showCur.num_of_ratings,
-            total_ratings: showCur.total_ratings,
-            average_ratings: showCur.average_ratings,
-            num_of_favorites: showCur.num_of_favorites,
-        }
-    }));
+   pageResults = results.slice(startIndex, endIndex)
+    res.status(200).json(pageResults);
   } else {
     res.status(400);
     throw new Error(
