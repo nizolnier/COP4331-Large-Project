3bbb40856fbdd76f2093b73839145840718f644c
Zain E. Yousaf Fuentes (CS @ UCF) <zainy0012@gmail.com>
2023-07-23 22:40:31 +0000

    added auth middleware to the shows routes

--- routes/shows.js
+++ routes/shows.js
@@ -1,59 +1,50 @@
 const requestUtilities = require("../utilities/requestUtilities")
 const Show = require("../models/showModel")
+const authMiddleware = require("../middlewares/auth")
 
 export default (app, routeBase) => {
-    app.get(`${routeBase}/all`, async (req, res) => {
-        if (!jwtUtilities.verifyAuthorizationRequest(req.headers)) {
-            res.status(401).send({
-                error: "Invalid authorization headers, invalid / non existing token data."
-            })
-        } else {
-            const shows = Show.find()
-            res.status(200).send(shows)
-        }
 
+
+    app.get(`${routeBase}/all`, authMiddleware, async (req, res) => {
+        const shows = await Show.find()
+        res.status(200).send(shows)
     })
 
-    app.get(`${routeBase}/id`, async (req, res) => {
-        if (!jwtUtilities.verifyAuthorizationRequest(req.headers)) {
-            res.status(401).send({
-                error: "Invalid authorization headers, invalid / non existing token data."
+
+    app.get(`${routeBase}/one`, authMiddleware, async (req, res) => {
+
+        const expectedParamKeys = [
+            "id"
+        ]
+
+        const missingParameterKeys = requestUtilities.validatedRequestObjectKeys(req.params, expectedParamKeys)
+
+        if (missingParameterKeys.length > 0) {
+            res.status(422).send({
+                error: `There are missing fields: (${missingParameterKeys.join(',')})`,
+                found: false
             })
         } else {
-            const expectedParamKeys = [
-                "id"
-            ]
-    
-            const missingParameterKeys = requestUtilities.validatedRequestObjectKeys(req.params, expectedParamKeys)
-    
-            if (missingParameterKeys.length > 0) {
-                res.status(422).send({
-                    error: `There are missing fields: (${missingParameterKeys.join(',')})`,
+            const {
+                id
+            } = req.params
+
+            const showExists = Show.findOne({ _id: mongoose.Types.ObjectId(id) })
+
+            if (!showExists) {
+                res.status(404).send({
+                    error: 'The show does not exist',
                     found: false
                 })
             } else {
-                const {
-                    id
-                } = req.params
-    
-                const showExists = Show.findOne({ _id: mongoose.Types.ObjectId(id) })
-    
-                if (!showExists) {
-                    res.status(404).send({
-                        error: 'The show does not exist',
-                        found: false
-                    })
-                } else {
-                    res.status(200).send({
-                        found: true,
-                        showExists
-                    })
-                }
+                res.status(200).send({
+                    found: true,
+                    showExists
+                })
             }
         }
 
     })
-
 
 
 }
