7a1351437bee88c2928a250dcb1a9b52a383179c
Zain E. Yousaf Fuentes (CS @ UCF) <zainy0012@gmail.com>
2023-07-23 22:13:49 +0000

    added auth middleware to reviews routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,139 +1,124 @@
 const requestUtilities = require("../utilities/requestUtilities")
 const jwtUtilities = require("../utilities/jwtUtilities")
 const Review = require("../models/reviewModel")
+const authMiddleware = require("../middlewares/auth")
 
 export default (app, routeBase) => {
-    app.post(`${routeBase}`, async (req, res, next) => {
-        if (!jwtUtilities.verifyAuthorizationRequest(req.headers)) {
-            res.status(401).send({
-                error: "Invalid authorization headers, invalid / non existing token data."
+
+
+    app.post(`${routeBase}`, authMiddleware, async (req, res, next) => {
+        const expectedBodyKeys = [
+            "showid",
+            "stars",
+            "favorite",
+            "comment",
+            "dateWatched"
+        ]
+
+        const missingBodyKeys = requestUtilities.validatedRequestObjectKeys(req.body, expectedBodyKeys)
+
+        if (missingBodyKeys.length > 0) {
+            res.status(422).send({
+                error: `There are missing fields: (${missingBodyKeys.join(',')})`,
+                created: false
             })
         } else {
-            const expectedBodyKeys = [
-                "showid",
-                "stars",
-                "favorite",
-                "comment",
-                "dateWatched"
-            ]
+            const { showid, stars, favorite, comment, dateWatched } = req.body
 
-            const missingBodyKeys = requestUtilities.validatedRequestObjectKeys(req.body, expectedBodyKeys)
+            const tokenData = jwtUtilities.getTokenData(req.header.auth)
 
-            if (missingBodyKeys.length > 0) {
-                res.status(422).send({
-                    error: `There are missing fields: (${missingBodyKeys.join(',')})`,
-                    created: false
-                })
-            } else {
-                const { showid, stars, favorite, comment, dateWatched } = req.body
+            const newReview = {
+                showid: mongoose.Types.ObjectId(showid),
+                userid: mongoose.Types.ObjectId(tokenData.id),
+                stars,
+                favorite,
+                comment,
+                dateWatched
+            }
 
-                const tokenData = jwtUtilities.getTokenData(req.header.auth)
-
-                const newReview = {
-                    showid: mongoose.Types.ObjectId(showid),
-                    userid: mongoose.Types.ObjectId(tokenData.id),
-                    stars,
-                    favorite,
-                    comment,
-                    dateWatched
-                }
-
-                try {
-                    await Review.create(newReview)
-                }
-                catch (e) {
-                    res.status(400).send({
-                        error: e.toString(),
-                        created: false
-                    })
-                }
+            try {
+                await Review.create(newReview)
 
                 res.status(201).send({
                     message: "User created",
                     created: true
                 })
-
             }
-        }
-    })
-
-    app.get(`${routeBase}/show`, async (req, res, next) => {
-        if (!jwtUtilities.verifyAuthorizationRequest(req.headers)) {
-            res.status(401).send({
-                error: "Invalid authorization headers, invalid / non existing token data."
-            })
-        } else {
-            const expectedParamKeys = [
-                "showid"
-            ]
-
-            const missingParameterKeys = requestUtilities.validatedRequestObjectKeys(req.params, expectedParamKeys)
-
-            if (missingParameterKeys.length > 0) {
-                res.status(422).send({
-                    error: `There are missing fields: (${missingParameterKeys.join(',')})`,
-                    found: false
+            catch (e) {
+                res.status(400).send({
+                    error: e.toString(),
+                    created: false
                 })
-            } else {
-                const {
-                    showid
-                } = req.params
-
-                const reviewExists = Review.findOne({ showid: mongoose.Types.ObjectId(showid) })
-
-                if (!reviewExists) {
-                    res.status(404).send({
-                        error: 'The review does not exist',
-                        found: false
-                    })
-                } else {
-                    res.status(200).send({
-                        found: true,
-                        reviewExists
-                    })
-                }
             }
 
         }
     })
 
-    app.get(`${routeBase}/user`, async (req, res, next) => {
-        if (!jwtUtilities.verifyAuthorizationRequest(req.headers)) {
-            res.status(401).send({
-                error: "Invalid authorization headers, invalid / non existing token data."
+
+    app.get(`${routeBase}/show`, authMiddleware, async (req, res, next) => {
+        const expectedParamKeys = [
+            "showid"
+        ]
+
+        const missingParameterKeys = requestUtilities.validatedRequestObjectKeys(req.params, expectedParamKeys)
+
+        if (missingParameterKeys.length > 0) {
+            res.status(422).send({
+                error: `There are missing fields: (${missingParameterKeys.join(',')})`,
+                found: false
             })
         } else {
-            const expectedParamKeys = [
-                "userid"
-            ]
+            const {
+                showid
+            } = req.params
 
-            const missingParameterKeys = requestUtilities.validatedRequestObjectKeys(req.params, expectedParamKeys)
+            const reviewExists = Review.findOne({ showid: mongoose.Types.ObjectId(showid) })
 
-            if (missingParameterKeys.length > 0) {
-                res.status(422).send({
-                    error: `There are missing fields: (${missingParameterKeys.join(',')})`,
+            if (!reviewExists) {
+                res.status(404).send({
+                    error: 'The review does not exist',
                     found: false
                 })
             } else {
-                const {
-                    userid
-                } = req.params
-
-                const reviewExists = Review.findOne({ userid: mongoose.Types.ObjectId(userid) })
-
-                if (!reviewExists) {
-                    res.status(404).send({
-                        error: 'The review does not exist',
-                        found: false
-                    })
-                } else {
-                    res.status(200).send({
-                        found: true,
-                        reviewExists
-                    })
-                }
+                res.status(200).send({
+                    found: true,
+                    reviewExists
+                })
             }
+        }
+    })
 
+
+    app.get(`${routeBase}/user`, authMiddleware, async (req, res, next) => {
+        const expectedParamKeys = [
+            "userid"
+        ]
+
+        const missingParameterKeys = requestUtilities.validatedRequestObjectKeys(req.params, expectedParamKeys)
+
+        if (missingParameterKeys.length > 0) {
+            res.status(422).send({
+                error: `There are missing fields: (${missingParameterKeys.join(',')})`,
+                found: false
+            })
+        } else {
+            const {
+                userid
+            } = req.params
+
+            const reviewExists = Review.findOne({ userid: mongoose.Types.ObjectId(userid) })
+
+            if (!reviewExists) {
+                res.status(404).send({
+                    error: 'The review does not exist',
+                    found: false
+                })
+            } else {
+                res.status(200).send({
+                    found: true,
+                    reviewExists
+                })
+            }
         }
     })
 
