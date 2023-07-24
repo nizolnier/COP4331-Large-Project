51e25e359d08851c637c16852b5a1267c2c3d88d
Zain E. Yousaf Fuentes (CS @ UCF) <zainy0012@gmail.com>
2023-07-24 00:02:36 +0000

    added paginated search support

--- routes/shows.js
+++ routes/shows.js
@@ -29,7 +29,7 @@ export default (app, routeBase) => {
                 id
             } = req.params
 
-            const showExists = Show.findOne({ _id: mongoose.Types.ObjectId(id) })
+            const showExists = await Show.findOne({ _id: mongoose.Types.ObjectId(id) })
 
             if (!showExists) {
                 res.status(404).send({
@@ -40,6 +40,66 @@ export default (app, routeBase) => {
                 res.status(200).send({
                     found: true,
                     showExists
+                })
+            }
+        }
+
+    })
+
+    app.get(`${routeBase}/search`, authMiddleware, async (req, res) => {
+
+        const expectedParamKeys = [
+            'input',
+            'pageIndex'
+        ]
+
+        const missingParameterKeys = requestUtilities.validatedRequestObjectKeys(req.params, expectedParamKeys)
+
+        if (missingParameterKeys.length > 0) {
+            res.status(422).send({
+                error: `There are missing fields: (${missingParameterKeys.join(',')})`,
+                found: false
+            })
+        } else {
+
+            const {
+                input,
+                pageIndex // Assume that this, based on handling on the frontend, is not allowed to be lower than 1
+            } = req.params
+
+            const likeInputRegex = new RegExp(`.*${input}.*`)
+
+
+            try {
+
+                // Would do the cool way with mongo operators but genre lowkey annoying to do for regex
+                const allShows = await Show.find()
+
+                const similarShows = allShows.filter(show => {
+                    return (
+                        likeInputRegex.test(show.title) ||
+                        likeInputRegex.test(show.genre.join()) ||
+                        likeInputRegex.test(show.description) ||
+                        likeInputRegex.test(show.year.toString()) ||
+                        likeInputRegex.test(show.director)
+                    )
+                })
+
+                // Using min and max for indexing safety
+                const showsForPage = similarShows.slice(
+                    Math.max(0, 10 * (pageIndex - 1)),
+                    Math.min(similarShows.length, 10 * pageIndex)
+                )
+
+                res.status(201).send({
+                    found: true,
+                    shows: showsForPage
+                })
+            } catch (e) {
+                // idk find eror nicole
+                res.status(400).send({
+                    error: e.toString(),
+                    found: false
                 })
             }
         }
