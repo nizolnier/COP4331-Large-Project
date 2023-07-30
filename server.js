a7f144fb53fd427cb290ed328e78d7a53b52cdc5
Nicole Zolnier <nzolnier@gmail.com>
2023-07-29 21:34:45 -0400

    modules are weird

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import setApp from './api.js'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+
+const __dirname = path.dirname(__filename)
 
 dotenv.config()
 
