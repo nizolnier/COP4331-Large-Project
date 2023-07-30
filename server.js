e46afc5ddee9f3ba5a900cc9a79edd8a967b48cb
Nicole Zolnier <nzolnier@gmail.com>
2023-07-29 21:25:39 -0400

    trying and catching

--- server.js
+++ server.js
@@ -45,9 +45,9 @@ const connectDB = async () => {
 connectDB()
 
 if (process.env.PROD) {
-    app.use(express.static('frontend/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    app.use(express.static(path.join(__dirname, 'frontend/build')))
+    app.get('/*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
     })
 }
 
